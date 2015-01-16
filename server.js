90a7dc925e612c069d7d5187b5767d9d61edfd62
Koushik Chhetri <koushikchhetri@users.noreply.github.com>
2015-01-16 12:30:34 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,14 +1,1 @@
-#Server.js is the heart beat of a realtime chat
 
-##Description
-
-server.js is a simple javascript code which resides in server. It communicates with the client througha a uniform specific socket port for all clients who are connected in the same server. Soket port may vary from 1001 to 1003 or so on. 
-
-To achieved realtime chat we need.
-  -Knowledge of node.js
-  - A socket server.
-  -
-  ```
-  <script src="https://cdn.socket.io/socket.io-1.2.1.js"></script> for client
-  ```
-  
